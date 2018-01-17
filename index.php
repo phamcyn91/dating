a56138e47ac34fa0ed3053473beaef160d3ba1fe
Cynthia Pham <cpham15@mail.greenriver.edu>
2018-01-16 20:53:02 -0500

    added default route that renders a view pages/home.html

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: phamcyn
+ * User: phamcyn (Cynthia Pham)
  * Date: 1/16/2018
  * Time: 4:45 PM
  */
+
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Set debug level (0-3)
+$f3->set('DEBUG', 3);
+
+//Default route to pages/home.html
+$f3->route('GET /', function() {
+    $view = new View;
+    echo $view->render
+    ('pages/home.html');
+}
+);
+
+//Run fat free
+$f3->run();
+
+?>
