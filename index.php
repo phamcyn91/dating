b2f20bb3121b8f86b9dc99ef07c28ab781a3621d
Cynthia Pham <cpham15@mail.greenriver.edu>
2018-01-19 15:47:00 -0500

    defined route for personal.html and profile.html

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: phamcyn (Cynthia Pham)
+ * User: Cynthia Pham
  * Date: 1/16/2018
  * Time: 4:45 PM
+ * index.php : Sets default route to pages/home.html
  */
 
 
@@ -21,6 +21,22 @@ $f3->route('GET /', function() {
     $view = new View;
     echo $view->render
     ('pages/home.html');
+}
+);
+
+//Defined route to pages/personal.html
+$f3->route('GET /personal', function() {
+    $view = new View;
+    echo $view->render
+    ('pages/personal.html');
+}
+);
+
+//Defined route to pages/profile.html
+$f3->route('POST /profile', function() {
+    $view = new View;
+    echo $view->render
+    ('pages/profile.html');
 }
 );
 
